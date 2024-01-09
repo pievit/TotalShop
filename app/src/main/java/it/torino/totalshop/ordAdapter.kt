91261e1f436fac87b85a6f6eca8eb252cca6b945
SavNin <saverio.ninivaggi@gmail.com>
2024-01-09 22:09:55 +0100

    Gestione Orders fragment venditore

---
+++ app/src/main/java/it/torino/totalshop/ordAdapter.kt
@@ -0,0 +1,53 @@
+package it.torino.totalshop
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import it.torino.totalshop.roomdb.entities.OrdersData
+import it.torino.totalshop.roomdb.entities.ProductsData
+import it.torino.totalshop.roomdb.entities.StoreData
+
+class ordAdapter(var ordList: List<OrdersData>, val onItemClick: (OrdersData) -> Unit) : RecyclerView.Adapter<ordAdapter.OrdViewHolder>(){
+
+    inner class OrdViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val stato : TextView = itemView.findViewById(R.id.statoOrd)
+        val countTot : TextView = itemView.findViewById(R.id.countTot)
+        val prezzoTot : TextView = itemView.findViewById(R.id.prezzoTot)
+        val dataOrd : TextView = itemView.findViewById(R.id.dataOrd)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrdViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.orders_list , parent , false)
+        return OrdViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: OrdViewHolder, position: Int) {
+        var c = ordList[position].listaProd.prods.size
+        var item = ordList[position]
+        holder.stato.text = ordList[position].status
+//        holder.dataOrd.text = ordList[position].data
+        holder.countTot.text = if (c > 1) {"$c articoli"} else {"$c articolo"}
+        var sum = ordList[position].listaProd.prods.sumOf { prod -> prod.price.toDouble() }
+        holder.prezzoTot.text = sum.toString() + "€"
+
+        holder.itemView.setOnClickListener{
+            onItemClick(item)
+        }
+    }
+
+
+
+    override fun getItemCount(): Int {
+        return ordList.size
+    }
+
+    fun setFilteredList(ordList: List<OrdersData>){
+        this.ordList = ordList
+        notifyDataSetChanged()
+    }
+
+
+}
--- app/src/main/java/it/torino/totalshop/venditore/OrdiniFragmentVenditore.kt
+++ app/src/main/java/it/torino/totalshop/venditore/OrdiniFragmentVenditore.kt
@@ -1,6 +1,81 @@
 package it.torino.totalshop.venditore
 
+import android.content.res.Configuration
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import it.torino.totalshop.R
+import it.torino.totalshop.ordAdapter
+import it.torino.totalshop.roomdb.entities.OrdersData
+import it.torino.totalshop.roomdb.entities.StoreData
+import it.torino.totalshop.viewModel
 
 class OrdiniFragmentVenditore : Fragment() {
+    var vm: viewModel? = null
+    private lateinit var recyclerView: RecyclerView
+    private var ordList = ArrayList<OrdersData>()
+    private lateinit var adapter: ordAdapter
+    private lateinit var myStore : StoreData
+
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        var view = inflater.inflate(R.layout.venditore_ordini, container, false)
+        vm = ViewModelProvider(requireActivity())[viewModel::class.java]
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        vm?.store
+
+        recyclerView = view.findViewById(R.id.ordersList)
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        adapter = ordAdapter(ordList) { selectedItem ->
+            if(isOrientationPortrait()){
+                //carica nel fragment
+//                val newDetails = DettagliFragmentVenditore.newInstance(myCourseCode)
+//                childFragmentManager.beginTransaction().replace(R.id.ordini_info, newDetails)
+//                setTransition(androidx.fragment.app.FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+//                commit()
+            } else {
+                val fragment2 : Fragment = DettagliFragmentVenditore()
+                with(childFragmentManager.beginTransaction()) {
+                    replace(R.id.ordiniInfoFragment, fragment2 )
+//                    arguments?.putBundle("OrderInfo", bun)
+                    setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                    commit()
+                }
+
+            }
+            Log.d("Item clicked: ",selectedItem.toString())
+        }
+        recyclerView.adapter = adapter
+        vm?.getStore(requireActivity().intent.getStringExtra("email")!!)
+        vm?.store?.observe(viewLifecycleOwner){
+                store ->
+            myStore = store
+            vm?.getAllOrdersFromStoreID(myStore.id)
+        }
+        vm!!.ordList.observe(viewLifecycleOwner){
+                pl ->
+            if(pl!=null){
+                ordList = pl as ArrayList<OrdersData>
+                adapter.setFilteredList(ordList)
+            }
+        }
+    }
+
+    private fun isOrientationPortrait(): Boolean {
+        val orientation = resources.configuration.orientation
+        return orientation == Configuration.ORIENTATION_PORTRAIT
+    }
+
 }
